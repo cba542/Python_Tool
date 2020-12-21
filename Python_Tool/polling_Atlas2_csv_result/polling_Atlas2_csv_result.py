ea78ce93975b66ec46bca6c460ea72927069e4cc
cheng_sam <cheng_sam@apple.com>
2020-12-21 14:55:37 +0800

    polling Atlas2 csv 1) record data for each DUT

--- Python_Tool/polling_Atlas2_csv_result/polling_Atlas2_csv_result.py
+++ Python_Tool/polling_Atlas2_csv_result/polling_Atlas2_csv_result.py
@@ -5,8 +5,7 @@ Created on Thu Oct 29 11:38:40 2020
 
 @author: cheng_sam
 """
-import re
-import datetime
+
 from pathlib import Path
 import os
 import csv
@@ -25,17 +24,23 @@ def checkCSV(row):
     lowerLimit = row[8]
     status = row[12]
     failureMessage = row[13]
-    # if "OverallTestResult" in subTestName:
+
+    #1) Check SS, SSP Presence Fail 2) exclude DataIntegrity and Throughput
     # if "PresenceDUTCheck"  and "SS" in testName and "Data" not in testName and "Throughput" not in testName:
-        # if status == "FAIL" : 
-            # return True
+    #     if status == "FAIL" : 
+    #         return True
+    #1) Check LS, FS, HS Presence Fail 2) exclude DataIntegrity and Throughput
+    if "PresenceDUTCheck"  and "LS" in testName and "Data" not in testName and "Throughput" not in testName:
+        if status == "FAIL" : 
+            return True
+        
     # if "CPort1UPUSBCAdapterVoltageTest5V" in testName :
         # if "DxVD" in keyName or "VD0R" in keyName:
             # return True
     # if "DxVD" in keyName  and "CPort1UPUSBCAdapterVoltageTest5V" in testName or "VD0R" in keyName:
     # if status == "PASS" :  
-    if status == "FAIL" :      
-        # if "Reset" not in testName:
+    # if status == "FAIL" :      
+        # if "Adap" in testName:
             # return True
     # if status == "FAIL" or status == "PASS":
     # if  Status == "FAIL" or "_ew" in subSubTestName and upLimit == "50" or "Through" in row[2]:
@@ -44,11 +49,15 @@ def checkCSV(row):
     # if "DyMa" in testName and status == "FAIL": 
     # if "Host" in testName:
     # if "ID0R failed to reached target current" in failureMessage:
-        return True
-    else:
-        return False
+        
+    #Check failed result
+    # if status == "FAIL" :
+        # return True
+    # else:
+    
+    return False
 
-def checkCSVPassFail(row):
+def checkCSVFail(row):
 
     status = row[12]
  
@@ -60,7 +69,11 @@ def checkCSVPassFail(row):
 Root_path = "/Users/cheng_sam/zhengh8x/Code/Python_Tool/Python_Tool/polling_Atlas2_csv_result/"
 Fail_Count = 0
 DUT_Count = 0
-DUT_Criterion_Match_Count = 0
+DUT_Condition_Match_Count = 0
+DUT_SN_Fail_Count_dict = {}
+DUT_SN_Count_dict = {}
+DUT_SN_Condition_Match_Count_dict = {}
+
 
 
 myfile =  open("result.txt", "w")
@@ -93,9 +106,8 @@ for r, d, f in os.walk(Root_path):
                         continue
                     
                 #To calculate the Fail Rate
-                if DUT_Status == 1:
-                    if checkCSVPassFail(row):
-                        DUT_Status = 0
+                if DUT_Status == 1 and checkCSVFail(row):
+                    DUT_Status = 0
                     
                 #condition in the csv want to check     
                 # if row[12] == "FAIL" or "_ew" in row[4] and row[6] == "50" or "Through" in row[2]:
@@ -108,33 +120,57 @@ for r, d, f in os.walk(Root_path):
                     myfile.write(result_str + "\n")
                     display = display + result_str
 
+            if SN not in DUT_SN_Condition_Match_Count_dict:
+                DUT_SN_Condition_Match_Count_dict[SN] = 0
+
             if len(display) > 0:
-                DUT_Criterion_Match_Count += 1
-                print(SN, "Match criterion")
+                DUT_Condition_Match_Count += 1
+                DUT_SN_Condition_Match_Count_dict[SN] = DUT_SN_Condition_Match_Count_dict[SN] + 1
+                print(SN, "Match Condition")
                 _FilePath = FilePath.replace("/Users/cheng_sam/zhengh8x/Code/Python_Tool/Python_Tool/polling_Atlas2_csv_result/","")
-                myfile.write("(" +  str(Fail_Count+1) + ")" + _FilePath + "\n")
+                myfile.write("(" +  str(DUT_Condition_Match_Count) + ")" + _FilePath + "\n")
                 myfile.write(("============================================================================================================================================================"+ "\n"))
             else:
-                print(SN, "Not Match criterion")
+                print(SN, "Not Match Condition")
+            
+            if SN not in DUT_SN_Fail_Count_dict:
+                DUT_SN_Fail_Count_dict[SN] = 0
+            if SN not in DUT_SN_Count_dict:
+                DUT_SN_Count_dict[SN] = 0
 
             #To calculate the Fail Rate
             if DUT_Status == 0:
                 Fail_Count += 1
+                DUT_SN_Fail_Count_dict[SN] = DUT_SN_Fail_Count_dict[SN] + 1
             DUT_Count += 1
-
+            DUT_SN_Count_dict[SN] = DUT_SN_Count_dict[SN] + 1
             
             csvFile.close()
 
             
 if DUT_Count is not 0:
-    print("%d Fail/%d Total, Match Rate = %f %%" % (Fail_Count, DUT_Criterion_Match_Count, (Fail_Count/DUT_Criterion_Match_Count) *100))
-    print("%d Fail/%d Total, Fail  Rate = %f %%" % (Fail_Count, DUT_Count, (Fail_Count/DUT_Count) *100))
-    myfile.write("\n%d Fail/%d Total, Match Rate = %f %%" % (Fail_Count, DUT_Criterion_Match_Count, (Fail_Count/DUT_Criterion_Match_Count) *100))
-    myfile.write("\n%d Fail/%d Total, Fail  Rate = %f %%" % (Fail_Count, DUT_Count, (Fail_Count/DUT_Count) *100))
+    print("%3d Fail/%3d Total, Match Rate = %f %%" % (DUT_Condition_Match_Count, DUT_Count, (DUT_Condition_Match_Count/DUT_Count) *100))
+    print("%3d Fail/%3d Total, Fail  Rate = %f %%" % (Fail_Count, DUT_Count, (Fail_Count/DUT_Count) *100))
+    myfile.write("Result Fail Rate Check")
+    myfile.write("\n%3d Fail/%3d Total, Match Rate = %f %%" % (DUT_Condition_Match_Count, DUT_Count, (DUT_Condition_Match_Count/DUT_Count) *100))
+    myfile.write("\n%3d Fail/%3d Total, Fail  Rate = %f %%" % (Fail_Count, DUT_Count, (Fail_Count/DUT_Count) *100))
     os.popen("open /Users/cheng_sam/zhengh8x/Code/Python_Tool/Python_Tool/polling_Atlas2_csv_result/result.txt")
+
+    myfile.write(("\n============================================================================================================================================================"+ "\n"))
+    myfile.write("DUTs Fail Rate Check")
+    for key in DUT_SN_Count_dict:
+        print("%s %dR/%dT" % (key, DUT_SN_Fail_Count_dict[key], DUT_SN_Count_dict[key]))
+        myfile.write("\n%s %dR/%dT" % (key, DUT_SN_Fail_Count_dict[key], DUT_SN_Count_dict[key]))
+    myfile.write(("\n============================================================================================================================================================"+ "\n"))
+    myfile.write("DUTs Match Condition Check")
+    for key in DUT_SN_Fail_Count_dict:
+        print("%s %dMT/%dT" % (key, DUT_SN_Condition_Match_Count_dict[key], DUT_SN_Count_dict[key]))
+        myfile.write("\n%s %dMT/%dT" % (key, DUT_SN_Condition_Match_Count_dict[key], DUT_SN_Count_dict[key]))
+    
+    
 else:
     print("No input csv")
-# csvFile.close()
+
 myfile.close()
     
 
